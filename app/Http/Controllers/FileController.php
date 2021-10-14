99deef0f1768586c6fe9e6f0a84101ec75a2da12
Gianni <gianni.brouwers@student.kdg.be>
2021-10-14 20:46:14 +0200

    updated image upload function

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -8,15 +8,29 @@ use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
+
     //
+    public function store(Request $request)
+    {
 
-    public function store(Request $request){
+        $note = new Note();
+        $note->id = 300;
+        $image = $note->addMediaFromRequest('upload')->toMediaCollection('images');
 
-        if($request->hasFile('upload')){
-        }
+        // $path_url = 'storage/' . Auth::id();
 
-
-
+        // if ($request->hasFile('upload')) {
+        //     $originName = $request->file('upload')->getClientOriginalName();
+        //     $fileName = pathinfo($originName, PATHINFO_FILENAME);
+        //     $extension = $request->file('upload')->getClientOriginalExtension();
+        //     $fileName = Str::slug($fileName) . '_' . time() . '.' . $extension;
+        //     $request->file('upload')->move(public_path($path_url), $fileName);
+        //     $url = asset($path_url . '/' . $fileName);
+        // }
+  
+        return response()->json([
+            'url' => $image->getUrl()
+        ]);
     }
 
 }
--- resources/views/subjects/notes/edit-note.blade.php
+++ resources/views/subjects/notes/edit-note.blade.php
@@ -57,120 +57,115 @@
 
                 <script>
 
-                        class MyUploadAdapter {
-                            constructor( loader ) {
-                                // The file loader instance to use during the upload.
-                                this.loader = loader;
-                            }
+                              class MyUploadAdapter {    
+                                constructor(loader) {
+                                    // The file loader instance to use during the upload. It sounds scary but do not
+                                    // worry — the loader will be passed into the adapter later on in this guide.
+                                    this.loader = loader;
 
-                            // Starts the upload process.
-                            upload() {
+                                    // URL where to send files.
+                                    this.url = '{{ route('image_upload') }}';
 
-                                console.log(server);
-
-                                // Update the loader's progress.
-                                server.onUploadProgress( data => {
-                                    loader.uploadTotal = data.total;
-                                    loader.uploaded = data.uploaded;
-                                } );
-
-                                // Return a promise that will be resolved when the file is uploaded.
-                                return loader.file
-                                    .then( file => server.upload( file ) );
-                            }
-
-                            // Aborts the upload process.
-                            abort() {
-                                // Reject the promise returned from the upload() method.
-                                server.abortUpload();
-                            }
-
-                            // Initializes the XMLHttpRequest object using the URL passed to the constructor.
-                            _initRequest() {
-                                const xhr = this.xhr = new XMLHttpRequest();
-
-                                // Note that your request may look different. It is up to you and your editor
-                                // integration to choose the right communication channel. This example uses
-                                // a POST request with JSON as a data structure but your configuration
-                                // could be different.
-                                xhr.open( 'POST', '{{ route('image_upload') }}', true );
-                                xhr.setRequestHeader('x-scrf-token', '{{csrf_token() }}');
-                                xhr.responseType = 'json';
-                            }
-
-
-                        // Initializes XMLHttpRequest listeners.
-                        _initListeners( resolve, reject, file ) {
-                                const xhr = this.xhr;
-                                const loader = this.loader;
-                                const genericErrorText = `Couldn't upload file: ${ file.name }.`;
-
-                                xhr.addEventListener( 'error', () => reject( genericErrorText ) );
-                                xhr.addEventListener( 'abort', () => reject() );
-                                xhr.addEventListener( 'load', () => {
-                                    const response = xhr.response;
-
-                                    // This example assumes the XHR server's "response" object will come with
-                                    // an "error" which has its own "message" that can be passed to reject()
-                                    // in the upload promise.
                                     //
-                                    // Your integration may handle upload errors in a different way so make sure
-                                    // it is done properly. The reject() function must be called when the upload fails.
-                                    if ( !response || response.error ) {
-                                        return reject( response && response.error ? response.error.message : genericErrorText );
+                                }
+                                // Starts the upload process.
+                                upload() {
+                                    return this.loader.file.then(
+                                        (file) =>
+                                            new Promise((resolve, reject) => {
+                                            this._initRequest();
+                                            this._initListeners(resolve, reject, file);
+                                            this._sendRequest(file);
+                                            })
+                                    );
+                                }
+                                // Aborts the upload process.
+                                abort() {
+                                    if (this.xhr) {
+                                        this.xhr.abort();
                                     }
+                                }
+                                // Initializes the XMLHttpRequest object using the URL passed to the constructor.
+                                _initRequest() {
+                                    const xhr = (this.xhr = new XMLHttpRequest());
+                                    // Note that your request may look different. It is up to you and your editor
+                                    // integration to choose the right communication channel. This example uses
+                                    // a POST request with JSON as a data structure but your configuration
+                                    // could be different.
+                                    // xhr.open('POST', this.url, true);
+                                    xhr.open("POST", this.url, true);
+                                    xhr.setRequestHeader("x-csrf-token", "{{ csrf_token() }}");
+                                    xhr.responseType = "json";
+                                }
+                                // Initializes XMLHttpRequest listeners.
+                                _initListeners(resolve, reject, file) {
+                                    const xhr = this.xhr;
+                                    const loader = this.loader;
+                                    const genericErrorText = `Couldn't upload file: ${file.name}.`;
+                                    xhr.addEventListener("error", () => reject(genericErrorText));
+                                    xhr.addEventListener("abort", () => reject());
+                                    xhr.addEventListener("load", () => {
+                                        const response = xhr.response;
 
-                                    // If the upload is successful, resolve the upload promise with an object containing
-                                    // at least the "default" URL, pointing to the image on the server.
-                                    // This URL will be used to display the image in the content. Learn more in the
-                                    // UploadAdapter#upload documentation.
-                                    resolve( {
-                                        default: response.url
-                                    } );
-                                } );
-
-
-                                // Upload progress when it is supported. The file loader has the #uploadTotal and #uploaded
-                                // properties which are used e.g. to display the upload progress bar in the editor
-                                // user interface.
-                                if ( xhr.upload ) {
-                                    xhr.upload.addEventListener( 'progress', evt => {
-                                        if ( evt.lengthComputable ) {
+                                        console.log(1);
+                                        console.log(xhr);
+                                        // This example assumes the XHR server's "response" object will come with
+                                        // an "error" which has its own "message" that can be passed to reject()
+                                        // in the upload promise.
+                                        //
+                                        // Your integration may handle upload errors in a different way so make sure
+                                        // it is done properly. The reject() function must be called when the upload fails.
+                                        if (!response || response.error) {
+                                            return reject(response);
+                                          //  return reject(response && response.error ? response.error.message : genericErrorText);
+                                        }
+                                        // If the upload is successful, resolve the upload promise with an object containing
+                                        // at least the "default" URL, pointing to the image on the server.
+                                        // This URL will be used to display the image in the content. Learn more in the
+                                        // UploadAdapter#upload documentation.
+                                        resolve({
+                                            default: response.url,
+                                        });
+                                    });
+                                    // Upload progress when it is supported. The file loader has the #uploadTotal and #uploaded
+                                    // properties which are used e.g. to display the upload progress bar in the editor
+                                    // user interface.
+                                    if (xhr.upload) {
+                                        xhr.upload.addEventListener("progress", (evt) => {
+                                            if (evt.lengthComputable) {
                                             loader.uploadTotal = evt.total;
                                             loader.uploaded = evt.loaded;
-                                        }
-                                    } );
+                                            }
+                                        });
+                                    }
                                 }
-                            }
+                                // Prepares the data and sends the request.
+                                _sendRequest(file) {
+                                    // Prepare the form data.
+                                    const data = new FormData();
+                                    data.append("upload", file);
+                                    // Important note: This is the right place to implement security mechanisms
+                                    // like authentication and CSRF protection. For instance, you can use
+                                    // XMLHttpRequest.setRequestHeader() to set the request headers containing
+                                    // the CSRF token generated earlier by your application.
+                                    // Send the request.
+                                    this.xhr.send(data);
+                                }
+                                // ...
+                                
+
+                                } // end of class
 
 
-                            // Prepares the data and sends the request.
-                            _sendRequest( file ) {
-                                // Prepare the form data.
-                                const data = new FormData();
+                    // default function:                         
+                    function CustomUploadAdapterPlugin( editor ) {
+                        editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
+                            // Configure the URL to the upload script in your back-end here!
+                            return new MyUploadAdapter( loader );
+                        };
+                    }
 
-                                data.append( 'upload', file );
-
-                                // Important note: This is the right place to implement security mechanisms
-                                // like authentication and CSRF protection. For instance, you can use
-                                // XMLHttpRequest.setRequestHeader() to set the request headers containing
-                                // the CSRF token generated earlier by your application.
-
-                                // Send the request.
-                                this.xhr.send( data );
-                            }
-
-
-                        } // end of class
-
-                        function CustomUploadAdapterPlugin( editor ) {
-                            editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
-                                // Configure the URL to the upload script in your back-end here!
-                                return new MyUploadAdapter( loader );
-                            };
-                        }
-
-
+                    // use custom adapter:
                     ClassicEditor
                         .create( document.querySelector( '#ck_editor' ), {
                             extraPlugins: [ CustomUploadAdapterPlugin ],
