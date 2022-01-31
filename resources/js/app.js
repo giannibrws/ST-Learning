9451e61c0f9fad154ac65730e96cbff2757abd04
Gianni <giannibrouwers@hotmail.com>
2022-01-31 12:57:57 +0100

    * Updated ckeditor - removed console error

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,18 +3,20 @@ require('alpinejs');
 
 console.log('Ready!')
 
+if (document.querySelector(".st-add-input")) {
 
-document.querySelector(".st-add-input").addEventListener("click", function() {
+    document.querySelector(".st-add-input").addEventListener("click", function() {
 
-    let name = document.querySelector("#name");
-    if(!name){
-        name =  document.querySelector("#sub_name");
-    }
-    if(name){
-        name.value = "";
-        // return cursor focus to the input field:
-        name.focus();
-    }
-});
+        let name = document.querySelector("#name");
+        if(!name){
+            name =  document.querySelector("#sub_name");
+        }
+        if(name){
+            name.value = "";
+            // return cursor focus to the input field:
+            name.focus();
+        }
+    });
 
+}
 
--- resources/views/subjects/notes/edit-note.blade.php
+++ resources/views/subjects/notes/edit-note.blade.php
@@ -57,7 +57,7 @@
 
                 <script>
 
-                    // default function:                         
+                    // default function:
                     function CustomUploadAdapterPlugin( editor ) {
                         editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
                             // Configure the URL to the upload script in your back-end here!
@@ -69,13 +69,28 @@
                     ClassicEditor
                         .create( document.querySelector( '#ck_editor' ), {
                             extraPlugins: [ CustomUploadAdapterPlugin ],
+                            image: {
+                                // You need to configure the image toolbar, too, so it uses the new style buttons.
+                                toolbar: [ 'imageTextAlternative', '|', 'imageStyle:alignLeft', 'imageStyle:full', 'imageStyle:alignRight' ],
+
+                                styles: [
+                                    // This option is equal to a situation where no style is applied.
+                                    'full',
+
+                                    // This represents an image aligned to the left.
+                                    'alignLeft',
+
+                                    // This represents an image aligned to the right.
+                                    'alignRight'
+                                ]
+                            }
                         } )
                         .catch( error => {
                             console.error( error );
                         });
 
                 </script>
-   
+
 </div>
 </div>
 </x-app-layout>
